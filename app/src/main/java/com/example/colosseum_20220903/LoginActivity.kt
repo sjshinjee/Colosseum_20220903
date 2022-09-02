5eb63e8bd89cc069940254fc56cc3d44f300576b
sjshinjee <110213136+sjshinjee@users.noreply.github.com>
2022-09-03 06:32:33 +0900

    서버유틸 작성을 직접 파일들에 작성중

--- app/src/main/java/com/example/colosseum_20220903/LoginActivity.kt
+++ app/src/main/java/com/example/colosseum_20220903/LoginActivity.kt
@@ -21,6 +21,7 @@ class LoginActivity : BaseActivity() {
             val eenputEmail = binding.emailEdt.text.toString()
             val eenputPw = binding.passwordEdt.text.toString()
 
+
 //         서버 유틸에서 적는걸 여기서 적는다고 생각하면 모든 페이지에 이렇게 적어야 한다(로직 보려고)
 ///        Request를 보내는 로직
         }
