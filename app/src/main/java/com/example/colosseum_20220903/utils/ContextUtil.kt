be3a948a5ccc4145563005236c8537a38aea1be1
sjshinjee <110213136+sjshinjee@users.noreply.github.com>
2022-09-05 04:15:17 +0900

    자동로그인 설정

--- app/src/main/java/com/example/colosseum_20220903/utils/ContextUtil.kt
+++ app/src/main/java/com/example/colosseum_20220903/utils/ContextUtil.kt
@@ -1,4 +1,40 @@
 package com.example.colosseum_20220903.utils
 
+import android.content.Context
+
 class ContextUtil {
+
+    companion object {
+
+        private val prefName = "Colosseum_Pref"
+
+        private val LOGIN_TOKEN = "LOGIN_TOKEN"
+        private val AUTO_LOGIN = "AUTO_LOGIN"
+
+        fun setLoginToken (context: Context, token : String){
+            val pref = context.getSharedPreferences(prefName, Context.MODE_PRIVATE)
+            pref.edit().putString(LOGIN_TOKEN, token).apply()
+        }
+
+        fun getLoginToken (context: Context) : String {
+            val pref = context.getSharedPreferences(prefName, Context.MODE_PRIVATE)
+            pref.getString(LOGIN_TOKEN, "")!!
+        }
+
+        fun setAutoLogin (context: Context, autoLogin : Boolean){
+            val pref = context.getSharedPreferences(prefName, Context.MODE_PRIVATE)
+            pref.edit().putBoolean(AUTO_LOGIN, autoLogin).apply()
+        }
+
+        fun getAutoLogin (context: Context) : Boolean {
+            val pref = context.getSharedPreferences(prefName, Context.MODE_PRIVATE)
+            return pref.getBoolean(AUTO_LOGIN, false)
+        }
+
+
+
+    }
+
+
+
 }
