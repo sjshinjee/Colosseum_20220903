8e0165e9c853f5482fe8d45d9732ec3459e58141
sjshinjee <110213136+sjshinjee@users.noreply.github.com>
2022-09-06 01:43:38 +0900

    리사이클러뷰 어댑터 토대 완성

--- app/src/main/java/com/example/colosseum_20220903/adapters/TopicRecyclerAdapter.kt
+++ app/src/main/java/com/example/colosseum_20220903/adapters/TopicRecyclerAdapter.kt
@@ -1,4 +1,35 @@
 package com.example.colosseum_20220903.adapters
 
-class TopicRecyclerAdapter {
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.colosseum_20220903.R
+import com.example.colosseum_20220903.datas.TopicData
+
+class TopicRecyclerAdapter(
+    val mContext: Context,
+    val mList: List<TopicData>
+) : RecyclerView.Adapter<TopicRecyclerAdapter.MyViewHolder>(){
+    inner class MyViewHolder(view : View) : RecyclerView.ViewHolder(view){
+        fun bind(item: TopicData){
+
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+        val row = LayoutInflater.from(mContext).inflate(R.layout.topic_list_item, parent,false)
+        return MyViewHolder(row)
+    }
+
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+        holder.bind(mList[position])
+    }
+
+    override fun getItemCount(): Int {
+        return mList.size
+    }
+
+
 }
