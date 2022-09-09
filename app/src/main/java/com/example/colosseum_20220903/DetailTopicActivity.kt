e734e1c93d80fa1295ad25275018776230c0d79a
sjshinjee <110213136+sjshinjee@users.noreply.github.com>
2022-09-09 13:49:10 +0900

    디테일토픽액티비티 xml일부 작성

---
+++ app/src/main/java/com/example/colosseum_20220903/DetailTopicActivity.kt
@@ -0,0 +1,11 @@
+package com.example.colosseum_20220903
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DetailTopicActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail_topic)
+    }
+}
