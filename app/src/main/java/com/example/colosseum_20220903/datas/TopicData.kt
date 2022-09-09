49ecac228972df427d62556a47023150eb193e55
sjshinjee <110213136+sjshinjee@users.noreply.github.com>
2022-09-09 13:55:59 +0900

    내 확인용

--- app/src/main/java/com/example/colosseum_20220903/datas/TopicData.kt
+++ app/src/main/java/com/example/colosseum_20220903/datas/TopicData.kt
@@ -1,8 +1,9 @@
 package com.example.colosseum_20220903.datas
 
 import org.json.JSONObject
+import java.io.Serializable
 
-class TopicData() {
+class TopicDat : Serializable {
 //원래는 TopicData()괄호 안에 멤버변수를 생성자로 표현했었으나 토픽데이터를 나중에 데이터 넣을거면 메인액티비티에서 넣어주야하므로
 ///멤버변수를 내부에 만들도록 한다 > 생성자는 기본 생성자 유지 멤버변수만 따로 추가해서 JSON을 파싱해서 변수에 넣는다
 //// 그 방법은 멤버변수는 서버의 데이터를 보고 담아두는 용도의 변수들로 만들면 된다
