55dd2f4702e70304460b5c2042b084af2c9d5100
sjshinjee <110213136+sjshinjee@users.noreply.github.com>
2022-09-11 21:07:01 +0900

    디테일토픽코틀린파일 바인딩 고쳐서

--- app/src/main/java/com/example/colosseum_20220903/DetailTopicActivity.kt
+++ app/src/main/java/com/example/colosseum_20220903/DetailTopicActivity.kt
@@ -2,10 +2,16 @@ package com.example.colosseum_20220903
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.example.colosseum_20220903.databinding.ActivityDetailTopicBinding
+import com.example.colosseum_20220903.databinding.ActivityMainBinding
 
 class DetailTopicActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityDetailTopicBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_detail_topic)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_detail_topic)
     }
 }
