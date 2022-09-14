ae025fab46fbd3f760c285281f739e943b28c383
sjshinjee <110213136+sjshinjee@users.noreply.github.com>
2022-09-14 15:43:04 +0900

    사이드 데이터 왜 에러뜨지

--- app/src/main/java/com/example/colosseum_20220903/DetailTopicActivity.kt
+++ app/src/main/java/com/example/colosseum_20220903/DetailTopicActivity.kt
@@ -3,16 +3,33 @@ package com.example.colosseum_20220903
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
+import com.bumptech.glide.Glide
 import com.example.colosseum_20220903.databinding.ActivityDetailTopicBinding
 import com.example.colosseum_20220903.databinding.ActivityMainBinding
+import com.example.colosseum_20220903.datas.TopicData
 
-class DetailTopicActivity : AppCompatActivity() {
+class DetailTopicActivity : BaseActivity() {
 
     lateinit var binding: ActivityDetailTopicBinding
+    lateinit var topicData : TopicData
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_detail_topic)
+
+        setupEvents()
+        setValues()
+    }
+
+    override fun setupEvents() {
+
+    }
+
+    override fun setValues() {
+        topicData = intent.getSerializableExtra("topicData") as TopicData
+
+        Glide.with(mContext).load(topicData.imageUrl).into(binding.backgroundImg)
+        binding.side1Txt
     }
 }
 
---
+++ app/src/main/java/com/example/colosseum_20220903/datas/SideData.kt
@@ -0,0 +1,24 @@
+package com.example.colosseum_20220903.datas
+
+import org.json.JSONObject
+import java.io.Serializable
+
+class SideData : Serializable {
+
+    var id = 0
+    var title = ""
+    var voteCount = 0
+
+    companion object{
+        fun getSideDataFromJsom (jsonObj: JSONObject) : SideData{
+            val sideData : SideData()
+            sideData.id = jsonObj.getInt("id")
+            sideData.title = jsonObj.getString("title")
+            sideData.id = jsonObj.getInt("vote_count")
+            return SideData()
+        }
+    }
+
+
+
+}
