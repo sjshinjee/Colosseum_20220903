026f93338c56895928503a428521cc9dbe35bbaf
sjshinjee <110213136+sjshinjee@users.noreply.github.com>
2022-09-15 00:09:40 +0900

    리사이클러뷰 실데이터 반영

--- app/src/main/java/com/example/colosseum_20220903/adapters/ReplyRecyclerAdapter.kt
+++ app/src/main/java/com/example/colosseum_20220903/adapters/ReplyRecyclerAdapter.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.colosseum_20220903.R
 import com.example.colosseum_20220903.datas.ReplyData
@@ -17,6 +18,27 @@ class ReplyRecyclerAdapter(
 
 
         fun bind (item : ReplyData){
+            val nickTxt = itemView.findViewById<TextView>(R.id.nickTxt)
+            val sideTxt = itemView.findViewById<TextView>(R.id.sideTxt)
+            val timeTxt = itemView.findViewById<TextView>(R.id.timeTxt)
+            val contentTxt = itemView.findViewById<TextView>(R.id.contentTxt)
+            val replyCountTxt = itemView.findViewById<TextView>(R.id.replyCountTxt)
+            val likeCountTxt = itemView.findViewById<TextView>(R.id.likeCountTxt)
+            val dislikeCountTxt = itemView.findViewById<TextView>(R.id.dislikeCountTxt)
+
+            nickTxt.text = item.user.nick
+            sideTxt.text = "(${item.selectedSide.title})"
+            timeTxt.text = item.createdAt   //후에 시간 양식으로 포맷
+            contentTxt.text = item.content
+            replyCountTxt.text = "답글 : ${item.replyCount}표"
+            likeCountTxt.text = "좋아요 : ${item.likeCount}표"
+            dislikeCountTxt.text = "싫어요 : ${item.disLikeCount}표"
+
+//          [시간 나면] 좋아요, 싫어요 버튼 클릭 이벤트 처리
+//          1 drawable생성 > red_border blud_border
+//          2 마이라이크/디스라이크의 속성(불린값)에 따라 좋아요>red_border 싫어요>blue_border
+//          3 클릭 이벤트 통해서 그 댓글의 좋아요 싫어요에 대한 서버 응답을 작성
+//          힌트, 뷰의 백그라운드리소스 변경
 
         }
     }
